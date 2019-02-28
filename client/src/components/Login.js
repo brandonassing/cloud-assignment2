bcccdc6cfc0376b8a12a431fb0fab989f09c4249
Brandon Assing <assing.brandon@gmail.com>
2019-02-28 17:03:18 -0500

    added login box

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,63 @@
 import React, { Component } from 'react';
 import './Login.css';
+// import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
 
 class Login extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: ""
+        }
+    }
+    handleUsernameChange = e => {
+        e.preventDefault();
+        this.setState({
+            username: e.target.value
+        });
+    }
+    handlePasswordChange = e => {
+        e.preventDefault();
+        this.setState({
+            password: e.target.value
+        });
+    }
     render() {
-        return(
-        <div>
-            login
-        </div>);
+        return (
+            <div id="login-body">
+                <div id="login-panel">
+                    <h2>Veeeeem</h2>
+                    <form noValidate autoComplete="off">
+                        <TextField
+                            id="filled-username"
+                            label="Username"
+                            value={this.state.username}
+                            onChange={this.handleUsernameChange}
+                            margin="normal"
+                            variant="filled"
+                            type="text"
+                            fullWidth={true}
+                        />
+                        <TextField
+                            id="filled-password"
+                            label="Password"
+                            value={this.state.password}
+                            onChange={this.handlePasswordChange}
+                            margin="normal"
+                            variant="filled"
+                            type="password"
+                            fullWidth={true}
+                        />
+                        <Button variant="outlined">
+                            Sign in
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        );
     }
 }
 
