1e97a7ac54bd43b7c4d47a191c9b3642509e79e2
Krishan Patel <krishanpatel09@gmail.com>
2019-03-04 16:52:24 -0500

    Added models for user and vm for server

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var User = require('../models/user');
+var User = require("../models/user");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// GET: return all users
+router.get("/", function(req, res) {
+  var query = {};
+  User.find({}, {}, query, function(err, data) {
+    // Mongo command to fetch all data from collection.
+    if (err) {
+      response = {
+        error: true,
+        message: "Error fetching data"
+      };
+    } else {
+      response = {
+        data: data
+      };
+    }
+    res.json(response);
+  });
 });
 
 module.exports = router;
