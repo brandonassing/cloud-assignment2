762a28a39828636cccb099aa0e80d257454cfc8a
Krishan Patel <krishanpatel09@gmail.com>
2019-03-04 17:05:08 -0500

    resolved conflicts with app.js in server

--- server/app.js
+++ server/app.js
@@ -7,18 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
-const mongoose = require("mongoose");
-
 var app = express();
-
-mongoose.Promise = global.Promise;
-mongoose.connect(
-  "mongodb+srv://main:se4455@main-gkrza.mongodb.net/test?retryWrites=true" ||
-    "mongodb://localhost:27017/post",
-  { useNewUrlParser: true }
-);
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
