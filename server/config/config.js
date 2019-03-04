aad4b6030d7678e1bbf688501a974c81fb376e8c
Brandon Assing <assing.brandon@gmail.com>
2019-03-04 18:54:18 -0500

    changed mongodb url

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 endpoints = {
     "cloud-usage-monitor": "http://localhost:4000",
-    "mongodb-url":"root address"
+    "mongodb-url":"mongodb://main:se4455@main-shard-00-00-gkrza.mongodb.net:27017,main-shard-00-01-gkrza.mongodb.net:27017,main-shard-00-02-gkrza.mongodb.net:27017/test?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true"
 }
 
 module.exports = endpoints
