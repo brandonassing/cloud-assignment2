ce05ddde63ea91a3c33b7e8e52f7dce7d9f951f4
Krishan Patel <krishanpatel09@gmail.com>
2019-03-04 18:59:25 -0500

    Connected with mongodb atlas

--- usage-monitor/app.js
+++ usage-monitor/app.js
@@ -1,52 +1,68 @@
-var createError = require("http-errors");
+// var createError = require("http-errors");
 var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var bodyParser = require("body-parser");
+// var path = require("path");
+// var cookieParser = require("cookie-parser");
+// var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+// var indexRouter = require("./routes/index");
+var userRouter = require("./routes/users");
+var vmRouter = require("./routes/vms");
 
 const mongoose = require("mongoose");
 
+const dbUri =
+  "mongodb://main:se4455@main-shard-00-00-gkrza.mongodb.net:27017,main-shard-00-01-gkrza.mongodb.net:27017,main-shard-00-02-gkrza.mongodb.net:27017/test?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true";
+
+const options = {
+  dbName: "Main",
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE,
+  poolSize: 10
+};
+
+mongoose.connect(dbUri, options).then(
+  () => {
+    console.log("Database connection established!");
+  },
+  err => {
+    console.log("Error connecting Database instance due to: ", err);
+  }
+);
+
 var app = express();
 
-mongoose.Promise = global.Promise;
-mongoose.connect(
-  "mongodb+srv://main:se4455@main-gkrza.mongodb.net/test?retryWrites=true" ||
-    "mongodb://localhost:27017/post",
-  { useNewUrlParser: true }
-);
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(logger("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// app.use("/", indexRouter);
+app.use("/users", userRouter);
+app.use("/vms", vmRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
 
 module.exports = app;
--- usage-monitor/routes/vms.js
+++ usage-monitor/routes/vms.js
@@ -1,27 +1,28 @@
 var express = require("express");
 var router = express.Router();
-const User = require("../models/User");
+const Vm = require("../models/Vm");
 
-// GET: Retrieve all users.
+// GET: Retrieve all vms.
 router.get("/", function(req, res, next) {
-  User.find({}, (err, user) => {
+  Vm.find({}, (err, vm) => {
     console.log("get request");
     if (err) {
       res.status(500).send(err);
     }
-    res.status(200).json(user);
+    res.status(200).json(vm);
   });
 });
 
+// POST: Create a VM
 router.post("/", function(req, res) {
   console.log("post request");
   console.log(req);
-  let newUser = new User();
-  newUser.save((err, user) => {
+  let newVm = new Vm();
+  newVm.save((err, vm) => {
     if (err) {
       res.status(500).send(err);
     }
-    res.status(201).json(user);
+    res.status(201).json(vm);
   });
 });
 
