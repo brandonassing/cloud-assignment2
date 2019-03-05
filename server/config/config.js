6f00978f873e1a6a2ef9b08f91054fd1cf431362
jraw96 <jtraw96@gmail.com>
2019-03-05 12:02:10 -0500

    Fixed routing issues to access the index.js in the bundled react app

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 endpoints = {
-    "cloud-usage-monitor": "http://localhost:4000",
+    "cloud-usage-monitor": "https://cloud-usage-monitor-cloud.appspot.com",
     "mongodb-url":"mongodb://main:se4455@main-shard-00-00-gkrza.mongodb.net:27017,main-shard-00-01-gkrza.mongodb.net:27017,main-shard-00-02-gkrza.mongodb.net:27017/test?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true"
 }
 
