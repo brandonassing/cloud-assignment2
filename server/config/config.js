25135a965256c1086bb59ac880b1269b3649b838
jraw96 <jtraw96@gmail.com>
2019-03-31 14:33:52 -0400

    inserted the correct post number for the mongo connection

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 endpoints = {
-  // "cloud-usage-monitor": "https://cloud-usage-monitor-cloud.appspot.com",
-  "cloud-usage-monitor": "http://localhost:4000",
+  "cloud-usage-monitor": "https://cloud-usage-monitor-cloud.appspot.com",
+  //"cloud-usage-monitor": "http://localhost:4000",
   "mongodb-url":
     "mongodb://main:se4455@main-shard-00-00-gkrza.mongodb.net:27017,main-shard-00-01-gkrza.mongodb.net:27017,main-shard-00-02-gkrza.mongodb.net:27017/test?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true"
 };
--- usage-monitor/app.js
+++ usage-monitor/app.js
@@ -13,7 +13,7 @@ var vmRouter = require("./routes/vms");
 //const dbUri = "mongodb://main:se4455@main-shard-00-00-gkrza.mongodb.net:27017,main-shard-00-01-gkrza.mongodb.net:27017,main-shard-00-02-gkrza.mongodb.net:27017/test?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true";
 
 // Cloud assignment 3 link: 
-const dbUri = "mongodb://main:se4455@10.0.0.5:27017:27017/test?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true";
+const dbUri = "mongodb://main:se4455@10.0.0.6:27017:27017/test?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true";
 
 
 
