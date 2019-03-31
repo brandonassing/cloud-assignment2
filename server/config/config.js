ad0057ec12f6c89f4d03f143752c5bfedfef9b05
jraw96 <jtraw96@gmail.com>
2019-03-31 15:02:42 -0400

    forgot to add http

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ endpoints = {
   //"mongodb-url":"mongodb://main:se4455@main-shard-00-00-gkrza.mongodb.net:27017,main-shard-00-01-gkrza.mongodb.net:27017,main-shard-00-02-gkrza.mongodb.net:27017/test?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true"
 
   // Assignment 3 endpoints: 
-  "cloud-usage-monitor": "10.0.0.5:4000",
+  "cloud-usage-monitor": "http://10.0.0.5:4000",
   "mongodb-url":"mongodb://10.0.0.6:27017/Main"
 
 };
