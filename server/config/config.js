0731493672d90cd3c2b3df0d177018b32a70d3cd
jraw96 <jtraw96@gmail.com>
2019-03-31 14:57:08 -0400

    updated the connection for server to cloud usage monitor vm

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,14 @@
 endpoints = {
-  "cloud-usage-monitor": "https://cloud-usage-monitor-cloud.appspot.com",
+
+  // Assignment 2 endpoints:
+ // "cloud-usage-monitor": "https://cloud-usage-monitor-cloud.appspot.com",
   //"cloud-usage-monitor": "http://localhost:4000",
-  "mongodb-url":
-    "mongodb://main:se4455@main-shard-00-00-gkrza.mongodb.net:27017,main-shard-00-01-gkrza.mongodb.net:27017,main-shard-00-02-gkrza.mongodb.net:27017/test?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true"
+  //"mongodb-url":"mongodb://main:se4455@main-shard-00-00-gkrza.mongodb.net:27017,main-shard-00-01-gkrza.mongodb.net:27017,main-shard-00-02-gkrza.mongodb.net:27017/test?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true"
+
+  // Assignment 3 endpoints: 
+  "cloud-usage-monitor": "10.0.0.5:4000",
+  "mongodb-url":"mongodb://10.0.0.6:27017/Main"
+
 };
 
 module.exports = endpoints;
