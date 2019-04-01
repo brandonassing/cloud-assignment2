1745cf59d00ce586ab5ce94dbf347f30aa148ff0
jraw96 <jtraw96@gmail.com>
2019-03-31 20:09:38 -0400

    changed the uris back to vms

--- usage-monitor/app.js
+++ usage-monitor/app.js
@@ -10,10 +10,10 @@ var userRouter = require("./routes/users");
 var vmRouter = require("./routes/vms");
 
 // Cloud Assignment 2 link
-const dbUri = "mongodb://main:se4455@main-shard-00-00-gkrza.mongodb.net:27017,main-shard-00-01-gkrza.mongodb.net:27017,main-shard-00-02-gkrza.mongodb.net:27017/test?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true";
+//const dbUri = "mongodb://main:se4455@main-shard-00-00-gkrza.mongodb.net:27017,main-shard-00-01-gkrza.mongodb.net:27017,main-shard-00-02-gkrza.mongodb.net:27017/test?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true";
 
 // Cloud assignment 3 link: 
-//const dbUri = "mongodb://10.0.0.6:27017/Main";
+const dbUri = "mongodb://10.0.0.6:27017/Main";
 
 
 
